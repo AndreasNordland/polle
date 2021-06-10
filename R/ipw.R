614981873c1dc102de2ee35accbe1a56193648f2
Andreas Nordland <andreas.nordland@maersk.com>
2021-06-10 09:11:28 +0200

    non-S3 functions

--- R/ipw.R
+++ R/ipw.R
@@ -1,32 +1,28 @@
 # TODO check robustness if not all actions are represented at each stage
 
 #' @export
-ipw <- function(object, g_models, g_functions, policy, g_full_history)
-  UseMethod("ipw")
-
-#' @export
-ipw.policy_data <- function(object, g_models = NULL, g_functions = NULL, policy, g_full_history = FALSE){
+ipw <- function(policy_data, g_models = NULL, g_functions = NULL, policy, g_full_history = FALSE){
   if (is.null(g_models) & is.null(g_functions)) stop("Either g-models or g-functions must be provided.")
   if (!is.null(g_functions) & !is.null(g_models)) stop("g-models and g-functions can not both be provided.")
 
-  K <- object$dim$K
-  action_set <- object$action_set
+  K <- policy_data$dim$K
+  action_set <- policy_data$action_set
 
   # getting the observed actions:
-  actions <- get_actions(object)
+  actions <- get_actions(policy_data)
 
   # getting the policy actions:
-  policy_actions <- policy(policy_data = object)
+  policy_actions <- policy(policy_data)
 
   # fitting the g-functions:
   if (!is.null(g_models)){
-    g_functions <- fit_g_functions(object, g_models, full_history = g_full_history)
+    g_functions <- fit_g_functions(policy_data, g_models, full_history = g_full_history)
   }
-  g_values <- evaluate(g_functions, object)
+  g_values <- evaluate(g_functions, policy_data)
   g_d_values <- get_a_values(a = policy_actions$d, action_set = action_set, g_values)
 
   # (n) vector with entries U_i:
-  U <- utility(object)$U
+  U <- utility(policy_data)$U
 
   # (n X K) matrix with entries I(d_k(H_k) = A_k):
   DA <- as.matrix(dcast(actions, id ~ stage, value.var = "A")[, -c("id"), with = FALSE])
@@ -41,7 +37,8 @@ ipw.policy_data <- function(object, g_models = NULL, g_functions = NULL, policy,
   out <- list(
     value_estimate = mean(phi_ipw),
     phi_ipw = phi_ipw,
-    g_functions = g_functions
+    g_functions = g_functions,
+    id = get_id(policy_data)
   )
 
   return(out)
--- R/or.R
+++ R/or.R
@@ -1,36 +1,24 @@
 # TODO: profile or
 
 #' @export
-or <- function(object, q_models, q_functions, policy, q_full_history)
-  UseMethod("or")
-
-#' @export
-or.policy_data <- function(object, q_models = NULL, q_functions = NULL, policy, q_full_history = FALSE){
+or <- function(policy_data, q_models = NULL, q_functions = NULL, policy, q_full_history = FALSE){
   if (is.null(q_models) & is.null(q_functions)) stop("Either q-models or q-functions must be provided.")
   if (!is.null(q_models) & !is.null(q_functions)) stop("q-models and q-functions can not both be provided.")
   if (!is.null(q_functions)){
     if(!(class(q_functions)[[1]] == "nuisance_functions")) stop("q-functions must be of class 'nuisance_functions'.")
   }
 
-  K <- object$dim$K
-  action_set <- object$action_set
+  K <- policy_data$dim$K
+  action_set <- policy_data$action_set
 
   # getting the policy actions
-  policy_actions <- policy(policy_data = object)
+  policy_actions <- policy(policy_data = policy_data)
 
   # fitting the q-functions
   if (!is.null(q_models)){
-    q_functions <- fit_Q_functions(object, policy_actions = policy_actions, q_models = q_models, full_history = q_full_history)
+    q_functions <- fit_Q_functions(policy_data, policy_actions = policy_actions, q_models = q_models, full_history = q_full_history)
   }
-  # else {
-  #   if (class(q_function)[[1]] == "list"){
-  #     stopifnot(length(q_function) == K)
-  #   }
-  #   if (class(q_function)[[1]] == "Q_function"){
-  #     q_function <- list(q_function)
-  #   }
-  # }
-  q_values <- evaluate(object = q_functions, policy_data = object)
+  q_values <- evaluate(policy_data = q_functions, policy_data = policy_data)
   q_d_values <- get_a_values(a = policy_actions$d, action_set = action_set, values = q_values)
 
   # (n X K) matrix with entries Q_k(d_k(H_k), H_k)
@@ -40,7 +28,8 @@ or.policy_data <- function(object, q_models = NULL, q_functions = NULL, policy, 
   out <- list(
     value_estimate = mean(phi_or),
     phi_or = phi_or,
-    q_functions = q_functions
+    q_functions = q_functions,
+    id = get_id(policy_data)
   )
 
   return(out)
