148d02a2316d618056df3093bd2aa6a4b436ed41
Andreas Nordland <andreas.nordland@maersk.com>
2021-06-11 13:15:31 +0200

    Update simulate_two_stage_data.R
    Using new wide to long function.

--- R/simulate_two_stage_data.R
+++ R/simulate_two_stage_data.R
@@ -1,66 +1,61 @@
 #' @import data.table
 
 #' @export
+simulate_two_stage_data_wide <- function(n, par, a_1, a_2){
+# stage 1
+L_1 <- par$mu_L[1] + rnorm(n = n, 0, par$sigma_L)
+C_1 <- par$gamma_C[1] * L_1 + par$mu_C[1] + rnorm(n = n, 0, par$sigma_C)
+U_1 <- L_1
+A_1 <- a_1(data.table(C_1 = C_1, L_1 = L_1, U_1 = U_1), par)
+
+
+# stage 2
+L_2 <- (par$mu_L[2] + rnorm(n = n, 0, par$sigma_L))
+C_2 <- (par$gamma_C[2] * L_1 + rnorm(n = n, 0, par$sigma_C))
+U_2 <- A_1 * (C_1 + L_2)
+A_2 <- a_2(data.table(C_1 = C_1, L_1 = L_1, A_1 = A_1, L_2 = L_2, C_2 = C_2, U_1 = U_1, U_2 = U_2), par)
+
+L_2 <- ifelse(A_1 == 1, L_2, NA)
+C_2 <- ifelse(A_1 == 1, C_2, NA)
+A_2 <- ifelse(A_1 == 1, A_2, NA)
+
+L_3 <- A_2 * (par$mu_L[3] + rnorm(n = n, 0, par$sigma_L))
+U_3 <- A_2 * (C_2 + L_3)
+L_3 <- ifelse(A_2 == 1, L_3, NA)
+
+data <- data.table(
+  L_1 = L_1,
+  C_1 = C_1,
+  A_1 = A_1,
+  L_2 = L_2,
+  C_2 = C_2,
+  A_2 = A_2,
+  L_3 = L_3,
+  U_1 = U_1,
+  U_2 = U_2,
+  U_3 = U_3
+)
+
+return(data)
+}
+
+#' @export
 simulate_two_stage_data <- function(n, par, a_1, a_2){
-  # stage 1
-  L_1 <- par$mu_L[1] + rnorm(n = n, 0, par$sigma_L)
-  C_1 <- par$gamma_C[1] * L_1 + par$mu_C[1] + rnorm(n = n, 0, par$sigma_C)
-  U_1 <- L_1
-  A_1 <- a_1(data.table(C_1 = C_1, L_1 = L_1, U_1 = U_1), par)
-
-
-  # stage 2
-  L_2 <- A_1 * (par$mu_L[2] + rnorm(n = n, 0, par$sigma_L))
-  C_2 <- A_1 * (par$gamma_C[2] * L_1 + rnorm(n = n, 0, par$sigma_C))
-  U_2 <- A_1 * (C_1 + L_2)
-  A_2 <- a_2(data.table(C_1 = C_1, L_1 = L_1, A_1 = A_1, L_2 = L_2, C_2 = C_2, U_1 = U_1, U_2 = U_2), par)
-
-  L_3 <- A_2 * (par$mu_L[3] + rnorm(n = n, 0, par$sigma_L))
-  U_3 <- A_2 * (C_2 + L_3)
-
-  # utility
-  U <- U_1 + U_2 + U_3
-
-  data <- data.table(
-    L_1 = L_1,
-    C_1 = C_1,
-    A_1 = A_1,
-    L_2 = L_2,
-    C_2 = C_2,
-    A_2 = A_2,
-    L_3 = L_3,
-    U_1 = U_1,
-    U_2 = U_2,
-    U_3 = U_3,
-    U = U
+  wide_data <- simulate_two_stage_data_wide(n = n, par = par, a_1 = a_1, a_2 = a_2)
+  A_cols <- c("A_1", "A_2")
+  X_cols <- list(
+    c("L_1", "L_2"),
+    c("C_1", "C_2")
   )
+  X_value_names <- c("L", "C")
+  U_cols <- c("U_1", "U_2", "U_3")
+  long_data <- wide_stage_data_to_long(wide_data, A_cols = A_cols, X_cols = X_cols, X_value_names = X_value_names, U_cols = U_cols)
 
-  data[, id := 1:.N]
-  col_a <- c("A_1", "A_2")
-  col_l <- c("L_1", "L_2", "L_3")
-  col_c <- c("C_1", "C_2")
-  col_u <- c("U_1", "U_2", "U_3")
-  stage_data <- melt(data, id.vars = c("id"), measure = list(col_a, col_l, col_c, col_u), value.name = c("A", "L", "C", "U"), variable.name = "stage")
-  stage_data[ , stage := as.numeric(as.character(stage))]
-  stage_data[, A := as.character(A)]
-  # set key
-  setkey(stage_data, id, stage)
+  long_data[event == 0, ("U_0") := 0]
+  long_data[event == 0, ("U_1") := C]
+  setindex(long_data, NULL)
 
-  # calculating U_1 and U_0
-  stage_data[, U_1 := C]
-  stage_data[, U_0 := 0]
-  # remove rows
-  stage_data[, L_lead := shift(L), id]
-  stage_data <- stage_data[!(stage == 3 & (L_lead == 0))]
-  stage_data[, L_lead := NULL]
-
-  stage_data[stage == 1, event := 0]
-  stage_data[(stage == 2) & (A == "1"), event := 0]
-  stage_data[(stage == 2) & (A == "0") & (L != 0), event := 0]
-  stage_data[(stage == 2) & (A == "0") & (L == 0), event := 1]
-  stage_data[stage == 3, event := 1]
-
-  return(stage_data)
+  return(long_data)
 }
 
 E_truncated_gauss <- function(mu, sigma, a, b){
