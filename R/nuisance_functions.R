09c8f6a555e0583a8a5ae68365e596287abec87a
Andreas Nordland <andreas.nordland@maersk.com>
2021-07-27 15:11:44 +0200

    Update nuisance_functions.R
    Removed evaluate.cross_fitted_nuisance_functions

--- R/nuisance_functions.R
+++ R/nuisance_functions.R
@@ -23,44 +23,43 @@ evaluate.nuisance_functions <- function(object, policy_data){
   return(values)
 }
 
-#' @export
-evaluate.cross_fitted_nuisance_functions <- function(object, policy_data){
-  id <- get_id(policy_data)
-  K <- policy_data$dim$K
-  action_set <- policy_data$action_set
-  full_history <- attr(object, "full_history")
-  folds <- attr(object, "folds")
-
-  if (length(object) == K){
-    stop()
-    history <- lapply(1:K, function(s) get_stage_history(policy_data, stage = s, full_history = full_history))
-    values <- mapply(history, object, FUN = function(h, f) evaluate(f, new_history = h), SIMPLIFY = FALSE)
-    values <- rbindlist(values)
-    setkey(values, id, stage)
-  } else if (length(object) == 1){
-    values <- mapply(
-      folds,
-      object[[1]],
-      FUN = function(fold, g_function){
-        validation_id <- id[fold]
-        validation_policy_data <- new_policy_data(
-          stage_data = policy_data$stage_data[id %in% validation_id],
-          baseline_data = policy_data$baseline_data[id %in% validation_id]
-        )
-        validation_history <- state_history(validation_policy_data)
-        values <- evaluate(g_function, new_history = validation_history)
-        return(values)
-      },
-      SIMPLIFY = FALSE
-    )
-    values <- rbindlist(values)
-    setkey(values, id, stage)
-  } else{
-    stop("Provide either 1 or K nuisance functions for evaluation.")
-  }
-
-  return(values)
-}
+# evaluate.cross_fitted_nuisance_functions <- function(object, policy_data){
+#   id <- get_id(policy_data)
+#   K <- policy_data$dim$K
+#   action_set <- policy_data$action_set
+#   full_history <- attr(object, "full_history")
+#   folds <- attr(object, "folds")
+#
+#   if (length(object) == K){
+#     stop()
+#     history <- lapply(1:K, function(s) get_stage_history(policy_data, stage = s, full_history = full_history))
+#     values <- mapply(history, object, FUN = function(h, f) evaluate(f, new_history = h), SIMPLIFY = FALSE)
+#     values <- rbindlist(values)
+#     setkey(values, id, stage)
+#   } else if (length(object) == 1){
+#     values <- mapply(
+#       folds,
+#       object[[1]],
+#       FUN = function(fold, g_function){
+#         validation_id <- id[fold]
+#         validation_policy_data <- new_policy_data(
+#           stage_data = policy_data$stage_data[id %in% validation_id],
+#           baseline_data = policy_data$baseline_data[id %in% validation_id]
+#         )
+#         validation_history <- state_history(validation_policy_data)
+#         values <- evaluate(g_function, new_history = validation_history)
+#         return(values)
+#       },
+#       SIMPLIFY = FALSE
+#     )
+#     values <- rbindlist(values)
+#     setkey(values, id, stage)
+#   } else{
+#     stop("Provide either 1 or K nuisance functions for evaluation.")
+#   }
+#
+#   return(values)
+# }
 
 #' @export
 cf_fit_nuisance_functions <- function(folds, fit_functions, policy_data, models, full_history = FALSE, ...){
