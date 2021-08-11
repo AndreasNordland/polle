a73e8a649d0b13a2c31bfc20e1caf45db7bd4d42
Klaus Holst <klaus@holst.it>
2021-08-11 15:03:53 +0200

    single stage sim

--- inst/sim/single_stage.R
+++ inst/sim/single_stage.R
@@ -1,9 +1,10 @@
 
 sim_single_stage <- function(n=1e4,
                              par=c(k = .1,  d = .5, a = 1, b = -2.5, c = 3),
-                             A_function=function(Z, L, B, k, d) {
-                               return(k*(Z + L - 1)*Z^(-2) + d*(B==1))
-                             },
+                             action_model=function(Z, L, B, k, d)
+                               return(k*(Z + L - 1)*Z^(-2) + d*(B==1)),
+                             utility_model=function(Z, L, A, a, b, c)
+                               Z + L + A*(c*Z + a*L + b),
                              seed=NULL,
                              return_model=FALSE, ...) {
   suppressPackageStartupMessages(require("lava"))
@@ -14,9 +15,9 @@ sim_single_stage <- function(n=1e4,
                                     uniform.lvm(),
                                     binomial.lvm(p=.3),
                                     binomial.lvm("logit"))
-  regression(m, ~A) <- A_function
-  regression(m, ~U) <-
-    function(Z, L, A, a, b, c)  Z + L + A*(c*Z + a*L + b)
+  regression(m, ~A) <- action_model
+  regression(m, ~U) <- utility_model
+
   if (return_model) return(m)
-  lava::sim(m, n)
+  lava::sim(m, n, p=par)
 }
