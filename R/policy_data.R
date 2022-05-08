5910d9fd2d524c6aa49bf424c084a3a63d42b613
Andreas Nordland <andreas.nordland@maersk.com>
2022-05-08 12:24:26 +0200

    Update policy_data

--- R/policy_data.R
+++ R/policy_data.R
@@ -3,13 +3,10 @@ new_policy_data <- function(stage_data, baseline_data = NULL, verbose){
   # checking and processing stage_data:
   {
     if (missing(stage_data)) stage_data <- NULL
-    if (is.null(stage_data)) stop("stage_data is missing or NULL.")
+    if (is.null(stage_data)) stop("'stage_data' is missing or NULL.")
     if (is.data.frame(stage_data)) stage_data <- as.data.table(stage_data)
-    if (!is.data.table(stage_data)) stop("stage_data must be a data.table or a data.frame.")
-    if (!all(c("id", "stage", "event", "A", "U") %in% colnames(stage_data))) stop("stage_data must contain id, stage, event, A (action) and U (utility/reward).")
-
-    # copying stage_data in order to avoid reference issues:
-    stage_data <- copy(stage_data)
+    if (!is.data.table(stage_data)) stop("'stage_data' must be a data.table or a data.frame.")
+    if (!all(c("id", "stage", "event", "A", "U") %in% colnames(stage_data))) stop("'stage_data' must contain id, stage, event, A (action) and U (utility/rewarsd).")
     setcolorder(stage_data, c("id", "stage", "event", "A"))
 
     # setting and checking keys:
@@ -19,22 +16,22 @@ new_policy_data <- function(stage_data, baseline_data = NULL, verbose){
     if (anyDuplicated(stage_data, by = key(stage_data)) > 0) stop("The combination of id & stage must be unique.")
     if (!all(stage_data[ , .(check = all(stage == 1:.N)), by = id]$check)) stop("stage must be on the form 1, 2, ..., k.")
 
-    # checking the event variable
+    # checking the event variable:
     if (any(is.na(stage_data$event))) stop("event have missing values.")
     if (!all(stage_data[, .(check = all(event == c(rep(0, times = (.N-1)), 1) | event == c(rep(0, times = (.N-1)), 2))), id]$check)) stop("event must be on the form 0,0,...,0,j (j in {1,2}).")
 
     # checking the action variable (A):
+    if (any(is.na(stage_data[event == 0, ]$A))) stop("'action' (A) has missing values.")
     if (!is.character(stage_data$A)){
-      message("Coercing A to type character.")
-      stage_data$A <- as.character(stage_data$A)
+      stage_data[, A := as.character(A)]
     }
 
     # getting the set of actions (A):
     action_set <- sort(unique(stage_data$A))
 
     # checking the utility variable (U):
-    if (!all(is.numeric(stage_data$U))) stop("The utility (U) must be numeric.")
-    if(any(is.na(stage_data$U))) stop("The utility (U) has missing values")
+    if (!all(is.numeric(stage_data$U))) stop("'utility' (U) must be numeric.")
+    if(any(is.na(stage_data$U))) stop("'utility' (U) has missing values")
 
     # checking the deterministic reward variables (U_A[.]):
     deterministic_reward_names <- paste("U_A", action_set, sep = "")
@@ -53,29 +50,10 @@ new_policy_data <- function(stage_data, baseline_data = NULL, verbose){
       stop(mes)
     }
 
-    # checking missing values for action stages (event == 0):
-    sd <- stage_data[event == 0, -c("id", "stage", "event", "A"), with = FALSE]
-    sd_names <- colnames(sd)
-    sdm <- sapply(sd, function(x) any(is.na(x)))
-    if (any(sdm)){
-      mes <- sd_names[sdm]
-      mes <- paste(mes, collapse = ", ")
-      mes <- paste(mes, " has missing values.", sep = "")
-      stop(mes)
-    }
-    rm(sd, sd_names, sdm)
-
-
-    # coercing columns of type factor to type character in stage_data:
+    # coercing variables of type factor to type character in stage_data:
     sdf <- sapply(stage_data, function(x) is.factor(x))
     if (any(sdf)){
       f_names <- colnames(stage_data)[sdf]
-      mes <- f_names
-      mes <- paste(mes, collapse = ", ")
-      mes <- paste("Coercing '", mes, "' to type character.", sep = "")
-      if (verbose == TRUE){
-        message(mes)
-      }
       stage_data[, (f_names) := lapply(.SD, as.character), .SDcols = f_names]
       rm(mes, f_names)
     }
@@ -87,42 +65,37 @@ new_policy_data <- function(stage_data, baseline_data = NULL, verbose){
   }
 
   # checking and processing baseline_data:
-  baseline_data_names <- NULL
-  if (!is.null(baseline_data)){
-    if (is.data.frame(baseline_data)) baseline_data <- as.data.table(baseline_data)
+  {
+    baseline_data_names <- NULL
+    if (!is.null(baseline_data)){
+      if (is.data.frame(baseline_data))
+        baseline_data <- as.data.table(baseline_data)
 
-    # copying baseline_data in order to avoid reference issues:
-    baseline_data <- copy(baseline_data)
+      # checking id:
+      if (!all(c("id") %in% names(baseline_data))) stop("'baseline_data' must contain id.")
+      if (any(is.na(baseline_data$id))) stop("'baseline_data' id has missing values")
+      if (anyDuplicated(baseline_data, by = key(baseline_data)) > 0) stop("'baseline_data' id contains duplicates.")
 
-    # checking id:
-    if (!all(c("id") %in% names(baseline_data))) stop("'baseline_data' must contain id.")
-    if (any(is.na(baseline_data$id))) stop("'baseline_data' id has missing values")
-    if (anyDuplicated(baseline_data, by = key(baseline_data)) > 0) stop("'baseline_data' id contains duplicates.")
+      # setting id as key:
+      setkey(baseline_data, id)
 
-    # setting id as key:
-    setkey(baseline_data, id)
+      # comparing id:
+      if (!all(unique(stage_data$id) == baseline_data$id)) stop("'baseline_data' id must match 'stage_data' id.")
 
-    # comparing id:
-    if (!all(unique(stage_data$id) == baseline_data$id)) stop("baseline_data id must match stage_data id.")
+      # coercing columns of type factor to type character in baseline_data:
+      bdf <- sapply(baseline_data, function(x) is.factor(x))
+      if (any(bdf)){
+        f_names <- colnames(baseline_data)[bdf]
+        baseline_data[, (f_names) := lapply(.SD, as.character), .SDcols = f_names]
+        rm(mes, f_names)
+      }
+      rm(bdf)
 
-    # Coercing columns of type factor to type character in baseline_data:
-    bdf <- sapply(baseline_data, function(x) is.factor(x))
-    if (any(bdf)){
-      f_names <- colnames(baseline_data)[bdf]
-      mes <- f_names
-      mes <- paste(mes, collapse = ", ")
-      mes <- paste("Coercing ", mes, " to type character.", sep = "")
-      if (verbose == TRUE)
-        message(mes)
-      baseline_data[, (f_names) := lapply(.SD, as.character), .SDcols = f_names]
-      rm(mes, f_names)
+      # getting the names of the baseline state data:
+      baseline_data_names <- names(baseline_data)[!(names(baseline_data) %in% c("id"))]
+    } else {
+      baseline_data <- stage_data[, .(id =unique(id))]
     }
-    rm(bdf)
-
-    # getting the names of the baseline state data:
-    baseline_data_names <- names(baseline_data)[!(names(baseline_data) %in% c("id"))]
-  } else {
-    baseline_data <- stage_data[, .(id =unique(id))]
   }
 
   # getting the dimensions:
@@ -163,7 +136,7 @@ new_policy_data <- function(stage_data, baseline_data = NULL, verbose){
 # The remaining columns are considered state variables (X).
 #
 
-#'
+
 #' Create Policy Data Object
 #'
 #' \code{policy_data} creates an object of class "policy_data".
@@ -171,7 +144,7 @@ new_policy_data <- function(stage_data, baseline_data = NULL, verbose){
 #' @param data [data.frame] or [data.table]; see Examples.
 #' @param baseline_data [data.frame] or [data.table]; see Examples.
 #' @param type Character string. If "wide", \code{data} is considered to be on wide format.
-#' Otherwise, \code{data} is considered to be on long format; see Examples.
+#' If "long", \code{data} is considered to be on long format; see Examples.
 #' @param action Action variable name(s). Character vector or character string.
 #' \itemize{
 #'   \item{} A vector is valid for wide data. The length of the vector determines the number of stages (K).
@@ -180,30 +153,82 @@ new_policy_data <- function(stage_data, baseline_data = NULL, verbose){
 #' @param covariates Stage specific covariate name(s). Character vector or named list of character vectors.
 #' \itemize{
 #'   \item{} A vector is valid for single stage wide data or long data.
-#'   \item{} A list is valid for multiple stage wide data. Each element of the list must be a character vector corresponding to the number of stages (K).
+#'   \item{} A named list is valid for multiple stage wide data. Each element must be a character vector with length K.
 #' }
 #' @param utility  Utility/Reward variable name(s). Character string or vector.
 #' \itemize{
   #'   \item{} A string is valid for long data and wide data with a single final utility.
-  #'   \item{} A vector is valid for wide data with incremental rewards. Must be of length K+1; see Examples.
+  #'   \item{} A vector is valid for wide data with incremental rewards. Must have length K+1; see Examples.
 #' }
-#' @param deterministic_rewards Deterministic reward variable name(s). Character vector or named list of character vectors.
-#' \itemize{
-#'   \item{}
-#'   \item{}
-#' }
+#' @param deterministic_rewards Deterministic reward variable name(s). Named list of character vectors of length K.
+#' The name of each element must be on the form "U_Aa" where "a" corresponds to an action in the action set.
 #' @details
 #' Each observation has the sequential form
 #' \deqn{O= {B, U_1, X_1, A_1, ..., U_K, X_K, A_K, U_{K+1}},}
 #' for a possibly stochastic number of stages K.
 #' \itemize{
-#'  \item{} B is a vector of baseline information.
+#'  \item{} B is a vector of baseline covariates.
 #'  \item{} U_k is the reward at stage k.
-#'  \item{} X_k is a vector summarizing the state at stage k.
+#'  \item{} X_k is a vector of covariates summarizing the state at stage k.
 #'  \item{} A_k is the categorical action at stage k.
 #' }
 #' The utility is given by the sum of the rewards, i.e., \eqn{U = sum_{k = 1}^{K+1} U_k}.
 #' @examples
+#' library("polle")
+#' ### Wide data: Single stage
+#' source(system.file("sim", "single_stage.R", package="polle"))
+#' par0 <- c(k = .1,  d = .5, a = 1, b = -2.5, c = 3, s = 1)
+#' d1 <- sim_single_stage(5e2, seed=1, par=par0); rm(par0)
+#' head(d1, 5)
+#' # constructing policy_data object:
+#' pd1 <- policy_data(d1, action="A", covariates=list("Z", "B", "L"), utility="U")
+#' pd1
+#' head(get_actions(pd1), 5)
+#' head(utility(pd1), 5)
+#' head(get_history(pd1)$AH, 5)
+#'
+#' ### Wide data: Two stages
+#' source(system.file("sim", "two_stage.R", package="polle"))
+#' par0 <- c(gamma = 0.5, beta = 1)
+#' d2 <- sim_two_stage(5e2, seed=1, par=par0); rm(par0)
+#' head(d2, 5)
+#' # constructing policy_data object:
+#' pd2 <- policy_data(d2,
+#'                   action = c("A_1", "A_2"),
+#'                   covariates = list(L = c("L_1", "L_2"),
+#'                                     C = c("C_1", "C_2")),
+#'                   utility = c("U_1", "U_2", "U_3"))
+#' pd2
+#' head(get_history(pd2, stage = 2)$AH, 5)
+#' head(get_history(pd2, stage = 2, full_history = T)$AH, 5)
+#'
+#' ### Long data: Multiple stages
+#' source(system.file("sim", "multi_stage.R", package="polle"))
+#' a_obs <- function(t, x, beta, ...){
+#'   prob <- lava::expit(beta[1] + (beta[2] * t) + (beta[3] * x))
+#'   rbinom(n = 1, size = 1, prob = prob)
+#' }
+#' par0 <- list(tau = 10,
+#'              lambda = c(0, -0.4, 0.3),
+#'              alpha =  c(0, 0.5, 0.1, -0.5, 0.4),
+#'              beta = c(0.3, 0, -0.5),
+#'              sigma = 1,
+#'              xi = 0.3,
+#'              psi = 1)
+#' d3 <- sim_multi_stage(5e2, par0, a = a_obs, seed = 1); rm(a_obs, par0)
+#' head(d3$stage_data, 10)
+#' # constructing policy_data object:
+#' pd3 <- policy_data(data = d3$stage_data,
+#'                    baseline_data = d3$baseline_data,
+#'                    type = "long",
+#'                    id = "id",
+#'                    stage = "stage",
+#'                    event = "event",
+#'                    action = "A",
+#'                    utility = "U")
+#' pd3
+#' head(get_history(pd3, stage = 3)$AH, 5)
+#' head(get_history(pd3, stage = 2, full_history = T)$AH, 5)
 #'
 #' @export
 policy_data <- function(data, baseline_data,
@@ -214,8 +239,10 @@ policy_data <- function(data, baseline_data,
                         id = NULL, stage = NULL, event = NULL,
                         verbose = FALSE) {
   data <- check_data(data)
+  data <- copy(data)
   if (!missing(baseline_data)){
     baseline_data <- check_data(baseline_data)
+    baseline_data <- copy(baseline_data)
   }
 
   type <- tolower(type)
@@ -288,7 +315,6 @@ check_data <- function(data){
 # Wide data ---------------------------------------------------------------
 
 melt_wide_data <- function(wide_data, id, action, covariates, baseline, utility, deterministic_rewards){
-  wide_data <- copy(wide_data)
 
   ### checking the form of the variable inputs
   # id:
@@ -353,10 +379,14 @@ melt_wide_data <- function(wide_data, id, action, covariates, baseline, utility,
         du <- unlist(du)
       if (!is.vector(du) | !is.character(du))
         stop("'deterministic_rewards' must be a character vector or a list of character vectors.")
+      if(length(du) != K){
+        mes <- paste("Each element in 'deterministic_rewards' must have length ", K, ".", sep = "")
+        stop(mes)
+      }
       return(du)
     })
     deterministic_reward_names <- paste("U_A", action_set, sep = "")
-    if ((!all(names(deterministic_rewards) %in% deterministic_reward_names)) | (is.null(deterministic_rewards) & is.null(names(deterministic_rewards)))) {
+    if ((!all(names(deterministic_rewards) %in% deterministic_reward_names)) | (!is.null(deterministic_rewards) & is.null(names(deterministic_rewards)))) {
       mes <- paste(deterministic_reward_names, collapse = "', '")
       mes <- paste("'deterministic_rewards' must be a named list with names in the set '", mes, "'.", sep = "")
       stop(mes)
@@ -499,7 +529,6 @@ format_long_data <- function(long_data, baseline_data, id, action, stage, event,
   rm(tmp)
 
   ### setting default variable names by reference in long_data:
-  long_data <- copy(long_data)
   set_default_name(long_data, variable_name = id, default_name = "id")
   set_default_name(long_data, variable_name = action, default_name = "A")
   set_default_name(long_data, variable_name = stage, default_name = "stage")
@@ -516,7 +545,6 @@ format_long_data <- function(long_data, baseline_data, id, action, stage, event,
   }
 
   if (!is.null(baseline_data)){
-    baseline_data <- copy(baseline_data)
     set_default_name(baseline_data, variable_name = id, default_name = "id")
   }
 
--- tests/testthat/test-policy_data.R
+++ tests/testthat/test-policy_data.R
@@ -1,3 +1,7 @@
+
+# check_data --------------------------------------------------------------
+
+
 test_that("check_data fails if not given a data.table with unique variables.", {
   data <- c("A", "B")
   expect_error(check_data(data), "'data' must be a data.table.")
@@ -8,12 +12,126 @@ test_that("check_data fails if not given a data.table with unique variables.", {
   expect_error(check_data(data), "'data' has duplicated variable names.")
 })
 
-test_that("policy_data melts wide data correctly for a single stage case.", {
+
+# policy_data wide data ---------------------------------------------------
+
+
+## two stage ---------------------------------------------------------------
+
+test_that("policy_data melts wide data correctly in a two stage case.", {
+  wide_data <- data.table(
+    B = c("gr1", "gr2"),
+    Z_1 = c("A", "B"),
+    L_1 = c(1,2),
+    Z_2 = c("C", "D"),
+    L_2 = c(3,4),
+    treat_1 = c(0,1),
+    treat_2 = c(1,0),
+    outcome = c(10, 5)
+  )
+
+  target_stage_data <- data.table(
+    id = c(1,1,1,2,2,2),
+    stage = c(1,2,3,1,2,3),
+    event = c(0,0,1,0,0,1),
+    A = as.character(c(0,1,NA,1,0,NA)),
+    Z = c("A","C",NA,"B","D",NA),
+    L = c(1,3,NA,2,4,NA),
+    U = c(0,0,10,0,0,5),
+    U_A0 = rep(0, 6),
+    U_A1 = rep(0, 6)
+  )
+  setkey(target_stage_data, id, stage)
+  setindex(target_stage_data, event)
+
+  pd <- policy_data(
+    data = wide_data,
+    action = c("treat_1", "treat_2"),
+    covariates = list(
+      Z = c("Z_1", "Z_2"),
+      L = c("L_1", "L_2")
+    ),
+    baseline = "B",
+    utility = "outcome"
+  )
+  expect_equal(pd$stage_data, target_stage_data)
+
+  # including deterministic rewards:
+  wide_data <- data.table(
+    B = c("gr1", "gr2"),
+    Z_1 = c("A", "B"),
+    L_1 = c(1,2),
+    Z_2 = c("C", "D"),
+    L_2 = c(3,4),
+    treat_1 = c(0,1),
+    treat_2 = c(1,0),
+    outcome_1 = c(10, 5),
+    outcome_2 = c(7, 3),
+    outcome_3 = c(-2, 1),
+    outcome_1_A0 = c(1.5, 2.5),
+    outcome_2_A0 = c(3.5, 4.5)
+  )
+
+  target_stage_data <- data.table(
+    id = c(1,1,1,2,2,2),
+    stage = c(1,2,3,1,2,3),
+    event = c(0,0,1,0,0,1),
+    A = as.character(c(0,1,NA,1,0,NA)),
+    Z = c("A","C",NA,"B","D",NA),
+    L = c(1,3,NA,2,4,NA),
+    U = c(10,7,-2,5,3,1),
+    U_A0 = c(1.5,3.5,NA,2.5,4.5,NA),
+    U_A1 = rep(0, 6)
+  )
+  setkey(target_stage_data, id, stage)
+  setindex(target_stage_data, event)
+
+  pd <- policy_data(
+    data = wide_data,
+    action = c("treat_1", "treat_2"),
+    covariates = list(
+      Z = c("Z_1", "Z_2"),
+      L = c("L_1", "L_2")
+    ),
+    baseline = "B",
+    utility = c("outcome_1", "outcome_2", "outcome_3"),
+    deterministic_rewards = list(U_A0 = c("outcome_1_A0", "outcome_2_A0"))
+  )
+  expect_equal(pd$stage_data, target_stage_data)
+
+  # invalid inputs:
+  args <- list(
+    data = wide_data,
+    action = c("treat_1", "treat_2"),
+    covariates = list(
+      Z = c("Z_1", "Z_2"),
+      L = c("L_1", "L_2")
+    ),
+    baseline = "B",
+    utility = c("outcome_1", "outcome_2", "outcome_3"),
+    deterministic_rewards = list(U_A0 = c("outcome_1_A0", "outcome_2_A0"))
+  )
+
+  args_copy <- args
+  args_copy$covariates$Z <- c("Z_1", "Z_1")
+  expect_error(do.call(what = "policy_data", args_copy), "Duplicated variables: \"Z_1\".")
+
+  args_copy <- args
+  args_copy$covariates$Z <- c("Z_1")
+  expect_error(do.call(what = "policy_data", args_copy), "Each element in 'covariates' must have length 2.")
+
+
+})
+
+
+## single stage ------------------------------------------------------------
+
+test_that("policy_data melts wide data correctly in a single stage case.", {
 
   wide_data <- data.table(
     B = c("gr1", "gr2"),
     Z = c("A", "B"),
-    L = c(1,2),
+    L = c(1, 2),
     treat = c(0,1),
     outcome = c(10, 5)
   )
@@ -89,7 +207,29 @@ test_that("policy_data melts wide data correctly for a single stage case.", {
   )
   rm(pd)
 
+  # deterministic rewards
+  wide_data_copy <- copy(wide_data)
+  wide_data_copy$reward_A0 <- 2
+  wide_data_copy$reward_A1 <- 1
+  expect_error(
+    policy_data(data = wide_data_copy, action = "treat", covariates = c("B", "Z", "L"), utility = "outcome", deterministic_rewards = list("reward_A0", "reward_A1")),
+    "'deterministic_rewards' must be a named list with names in the set 'U_A0', 'U_A1'."
+  )
+  expect_error(
+    policy_data(data = wide_data_copy, action = "treat", covariates = c("B", "Z", "L"), utility = "outcome", deterministic_rewards = list(U_0 = "reward_A0", U_1 = "reward_A1")),
+    "'deterministic_rewards' must be a named list with names in the set 'U_A0', 'U_A1'."
+  )
+  pd <- policy_data(data = wide_data_copy, action = "treat", covariates = c("B", "Z", "L"), utility = "outcome", deterministic_rewards = list(U_A0 = "reward_A0", U_A1 = "reward_A1"))
+  expect_identical(
+    pd$stage_data[, c("U_A0", "U_A1"), with = FALSE],
+    data.table(U_A0 = c(2,NA,2,NA), U_A1 = c(1,NA,1,NA))
+  )
 })
+
+
+# policy_data long data ---------------------------------------------------------------
+
+## single stage ------------------------------------------------------------
 
 test_that("policy_data formats long data correctly for a single stage case.", {
 
@@ -217,3 +357,50 @@ test_that("policy_data formats long data correctly for a single stage case.", {
   )
 
 })
+
+# new_policy_data missing values ------------------------------------------
+
+test_that("policy_data handles missing values.", {
+  # long data:
+  ld <- data.table(
+    id = c(1,1,2,2),
+    stage = c(1,2,1,2),
+    event = c(0,1,0,1),
+    A = c(0, NA, 1, NA),
+    B = c("gr1", NA, "gr2", NA),
+    Z = c("A", NA, "B", NA),
+    L = c(1, NA, 2, NA),
+    U = c(0, 10, 0, 5),
+    U_A0 = rep(0, 4),
+    U_A1 = rep(0, 4)
+  )
+  setkey(ld, id, stage)
+  setindex(ld, event)
+
+  # baseline data:
+  bd <- data.table(
+    id = c(1,2),
+    W = c("blue", "red")
+  )
+  setkey(bd, id)
+
+  # correct
+  expect_error(
+    policy_data(data = ld, baseline_data = bd, type = "long"),
+    NA
+  )
+
+  # missing values:
+  ld$A <- c(NA , NA, 1, NA)
+  expect_error(policy_data(data = ld, baseline_data = bd, type = "long"))
+  ld$A <- c(0, NA, 1, NA)
+
+  ld$U <- c(NA, 10, 0, 5)
+  expect_error(policy_data(data = ld, baseline_data = bd, type = "long"))
+  ld$U <- c(0, 10, 0, 5)
+
+  ld$Z <- c(NA, NA, "B", NA)
+  expect_error(policy_data(data = ld, baseline_data = bd, type = "long"), NA) # allowing for missing covariates
+})
+
+
