14f65366c8796a4cb1473a3e528f1a9ee478073f
Andreas-Nordland <72131629+Andreas-Nordland@users.noreply.github.com>
2022-09-07 12:56:32 +0200

    Update policy_eval_test.R

--- tests/policy_eval_test.R
+++ tests/policy_eval_test.R
@@ -68,123 +68,97 @@ pe1 <- list(
                        M = 2)
 )
 
-
 tmp <- pe1
+
+# all(unlist(lapply(tmp, coef)) == unlist(lapply(pe1, coef)))
+# all(unlist(lapply(tmp, IC)) == unlist(lapply(pe1, IC)))
 
 
 
 # Two stage ---------------------------------------------------------------
 
-
-
-
 source(system.file("sim", "two_stage.R", package="polle"))
-par0 <- c(gamma = 0.5, beta = 1)
-d <- sim_two_stage(2e3, seed=1, par=par0)
-pd <- policy_data(d,
-                  action = c("A_1", "A_2"),
-                  covariates = list(L = c("L_1", "L_2"),
-                                    C = c("C_1", "C_2")),
-                  utility = c("U_1", "U_2", "U_3"))
+d2 <- sim_two_stage(1e3, seed=1)
+pd2 <- policy_data(d2,
+                   action = c("A_1", "A_2"),
+                   baseline = c("B"),
+                   covariates = list(L = c("L_1", "L_2"),
+                                     C = c("C_1", "C_2")),
+                   utility = c("U_1", "U_2", "U_3"))
 
-
-tmp <- polle:::fit_g_functions(
-  policy_data = pd,
-  g_models = g_rf(num.trees = c(500, 750)),
-  full_history = FALSE
+# policies
+pl2_dynamic <- policy_def(
+  policy_functions = list(dynamic_policy(function(L_1) (L_1>0)),
+                          dynamic_policy(function(C_2) (C_2>0))),
+  reuse = FALSE
 )
-
-tmp$all_stages$g_model
-
-evaluate(tmp, pd)
-
-
-
-pl <- policy_learn(type = "rqvl",
-                   qv_models = list(q_glm(~C_1), q_glm(~C_1+C_2)),
-                   qv_full_history = TRUE,
-                   L = 3,
-                   save_cross_fit_models = TRUE)
-
-
-
-# pl <- policy_learn(type = "ptl",
-#                    policy_vars = list(c("C_1"), c("C_2", "C_2")),
-#                    policy_full_history = TRUE,
-#                    L = 3,
-#                    save_cross_fit_models = TRUE)
-
-q_functions <- polle:::fit_Q_functions(
-  policy_data = pd,
-  policy_actions = pl_static(pd),
-  q_models = list(q_glm(), q_glm()),
-  full_history = TRUE
-)
-
 
 set.seed(1)
-pe_ipw <- policy_eval(type = "ipw",
-                  policy_data = pd,
-                  policy_learn = pl,
-                  q_models = q_glm(),
-                  g_models = g_glm(),
-                  M = 2)
+pe2 <- list(
+  ipw =  policy_eval(type = "ipw",
+                     policy_data = pd2,
+                     policy = pl2_dynamic,
+                     g_models = g_glm(),
+                     M = 1),
+  or = policy_eval(type = "or",
+                   policy_data = pd2,
+                   policy = pl2_dynamic,
+                   q_models = q_glm(),
+                   M = 1),
+  dr = policy_eval(type = "dr",
+                   policy_data = pd2,
+                   policy = pl2_dynamic,
+                   g_models = g_glm(),
+                   q_models = q_glm(),
+                   M = 1),
+  ipw_full =  policy_eval(type = "ipw",
+                          policy_data = pd2,
+                          policy = pl2_dynamic,
+                          g_models = list(g_glm(), g_glm()),
+                          g_full_history = TRUE,
+                          M = 1),
+  or_full = policy_eval(type = "or",
+                        policy_data = pd2,
+                        policy = pl2_dynamic,
+                        q_models = list(q_glm(), q_glm()),
+                        q_full_history = TRUE,
+                        M = 1),
+  dr_full = policy_eval(type = "dr",
+                        policy_data = pd2,
+                        policy = pl2_dynamic,
+                        g_models = list(g_glm(), g_glm()),
+                        g_full_history = TRUE,
+                        q_models = list(q_glm(), q_glm()),
+                        q_full_history = TRUE,
+                        M = 1),
+  ipw_cross =  policy_eval(type = "ipw",
+                           policy_data = pd2,
+                           policy = pl2_dynamic,
+                           g_models = list(g_glm(), g_glm()),
+                           g_full_history = TRUE,
+                           q_models = list(q_glm(), q_glm()),
+                           q_full_history = TRUE,
+                           M = 2),
+  or_cross = policy_eval(type = "or",
+                         policy_data = pd2,
+                         policy = pl2_dynamic,
+                         g_models = list(g_glm(), g_glm()),
+                         g_full_history = TRUE,
+                         q_models = list(q_glm(), q_glm()),
+                         q_full_history = TRUE,
+                         M = 2),
+  dr_cross = policy_eval(type = "dr",
+                         policy_data = pd2,
+                         policy = pl2_dynamic,
+                         g_models = list(g_glm(), g_glm()),
+                         g_full_history = TRUE,
+                         q_models = list(q_glm(), q_glm()),
+                         q_full_history = TRUE,
+                         M = 2)
+)
 
-pe_or <- policy_eval(type = "or",
-                      policy_data = pd,
-                      policy_learn = pl,
-                      q_models = q_glm(),
-                      g_models = g_glm(),
-                      M = 2)
+tmp2 <- pe2
 
-pe_dr <- policy_eval(type = "dr",
-                     policy_data = pd,
-                     policy_learn = pl,
-                     q_models = q_glm(),
-                     g_models = list(g_glm(), g_glm()),
-                     g_full_history = TRUE,
-                     M = 2)
-
-# set.seed(1)
-# policy_eval(type = "dr",
-#             policy_data = pd,
-#             policy = pl_static,
-#             q_functions = q_functions,
-#             q_models = q_glm(),
-#             g_models = g_glm(),
-#             M = 2)
-
-
-# set.seed(1)
-# pe_ipw_new <- policy_eval(type = "ipw",
-#                       policy_data = pd,
-#                       policy_learn = pl,
-#                       q_models = q_glm(),
-#                       g_models = g_glm(),
-#                       M = 2)
-#
-#
-# pe_or_new <- policy_eval(type = "or",
-#                      policy_data = pd,
-#                      policy_learn = pl,
-#                      q_models = q_glm(),
-#                      g_models = g_glm(),
-#                      M = 2)
-#
-# pe_dr_new <- policy_eval(type = "dr",
-#                      policy_data = pd,
-#                      policy_learn = pl,
-#                      q_models = q_glm(),
-#                      g_models = g_glm(),
-#                      M = 2)
-#
-#
-# pe_ipw_new$type <- NULL
-# all.equal(pe_ipw, pe_ipw_new) # remove type in output of policy_eval_type
-#
-# pe_or_new$type <- NULL
-# all.equal(pe_or, pe_or_new) # remove type in output of policy_eval_type
-#
-# pe_dr_new$type <- NULL
-# all.equal(pe_dr, pe_dr_new) # remove type in output of policy_eval_type
+# all(unlist(lapply(tmp, coef)) == unlist(lapply(pe1, coef)))
+# all(unlist(lapply(tmp, IC)) == unlist(lapply(pe1, IC)))
 
