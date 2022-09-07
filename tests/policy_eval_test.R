9cea50a81d0e8ba6ddd218bbfa2872a8f952375d
Andreas-Nordland <72131629+Andreas-Nordland@users.noreply.github.com>
2022-09-07 15:25:51 +0200

    Update policy_eval_test.R

--- tests/policy_eval_test.R
+++ tests/policy_eval_test.R
@@ -75,7 +75,7 @@ pe1 <- list(
 
 
 
-# Two stage ---------------------------------------------------------------
+# Two stages ---------------------------------------------------------------
 
 source(system.file("sim", "two_stage.R", package="polle"))
 d2 <- sim_two_stage(1e3, seed=1)
@@ -161,4 +161,63 @@ pe2 <- list(
 
 # all(unlist(lapply(tmp, coef)) == unlist(lapply(pe1, coef)))
 # all(unlist(lapply(tmp, IC)) == unlist(lapply(pe1, IC)))
+
+# Multiple stages ---------------------------------------------------------
+
+source(system.file("sim", "multi_stage.R", package="polle"))
+d3 <- sim_multi_stage(2e3, seed = 1)
+# constructing policy_data object:
+pd3 <- policy_data(data = d3$stage_data,
+                   baseline_data = d3$baseline_data,
+                   type = "long")
+pd3 <- partial(pd3, K = 2)
+
+# policies
+pl3_dynamic <- policy_def(
+  policy_functions = dynamic_policy(function(X) (X>0)),
+  reuse = TRUE
+)
+
+get_history_names(pd3, stage = 2)
+
+pe3 <- list(
+  ipw =  policy_eval(type = "ipw",
+                     policy_data = pd3,
+                     policy = pl3_dynamic,
+                     g_models = g_glm(),
+                     M = 1),
+  or = policy_eval(type = "or",
+                   policy_data = pd3,
+                   policy = pl3_dynamic,
+                   q_models = q_glm(),
+                   M = 1),
+  dr = policy_eval(type = "dr",
+                   policy_data = pd3,
+                   policy = pl3_dynamic,
+                   g_models = g_glm(),
+                   q_models = q_glm(),
+                   M = 1),
+  ipw_full =  policy_eval(type = "ipw",
+                     policy_data = pd3,
+                     policy = pl3_dynamic,
+                     g_models = list(g_glm(~ t_1 + X_1), g_glm(~ t_2 + X_2)),
+                     g_full_history = TRUE,
+                     M = 2),
+  or_full = policy_eval(type = "or",
+                   policy_data = pd3,
+                   policy = pl3_dynamic,
+                   q_models = list(q_glm(~ t_1 + X_1 + B), q_glm(~ t_2 + X_2 + B)),
+                   q_full_history = TRUE,
+                   M = 2),
+  dr_full = policy_eval(type = "dr",
+                   policy_data = pd3,
+                   policy = pl3_dynamic,
+                   g_models = list(g_glm(~ t_1 + X_1), g_glm(~ t_2 + X_2)),
+                   g_full_history = TRUE,
+                   q_models = list(q_glm(~ t_1 + X_1 + B), q_glm(~ t_2 + X_2 + B)),
+                   q_full_history = TRUE,
+                   M = 2)
+)
+
+tmp3 <- pe3
 
