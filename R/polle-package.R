6865b137052d82cf274f67860d3dc277824ccbbd
Klaus Holst <klaus@holst.it>
2022-10-18 17:24:37 +0200

    imports

--- R/polle-package.R
+++ R/polle-package.R
@@ -3,12 +3,16 @@
 ##'
 ##' @name polle-package
 ##' @importFrom lava IC estimate
+##' @importFrom stats predict gaussian binomial glm as.formula nls model.frame
+##'   model.matrix reformulate predict.glm terms update .getXlevels addmargins
+##'   coef delete.response complete.cases
+##' @importFrom methods formalArgs
+##' @importFrom utils tail capture.output
+##' @importFrom survival survfit
 ##' @import data.table
 ##' @aliases polle-package polle
 ##' @docType package
 ##' @keywords package
-##' @examples
-##'
 NULL
 
 ##' For internal use
