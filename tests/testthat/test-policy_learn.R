c8ef196bda01b0b4d5c27f502409acff4a9f5c80
Andreas-Nordland <72131629+Andreas-Nordland@users.noreply.github.com>
2022-10-31 10:18:15 +0100

    Update test-policy_learn.R

--- tests/testthat/test-policy_learn.R
+++ tests/testthat/test-policy_learn.R
@@ -1,20 +1,29 @@
 
 # Two stages --------------------------------------------------------------
 
-source(system.file("sim", "two_stage.R", package="polle"))
-d <- sim_two_stage(2e3, seed=1)
-pd <- policy_data(d,
-                  action = c("A_1", "A_2"),
-                  baseline = c("BB", "B"),
-                  covariates = list(L = c("L_1", "L_2"),
-                                    C = c("C_1", "C_2")),
-                  utility = c("U_1", "U_2", "U_3"))
+
 
 ## QV-learning -------------------------------------------------------------
 
 test_that("input to policy_learn with type rqvl handles incorrect arguments",{
+
+  source(system.file("sim", "two_stage.R", package="polle"))
+  d <- sim_two_stage(2e3, seed=1)
+  pd <- policy_data(d,
+                    action = c("A_1", "A_2"),
+                    baseline = c("BB", "B"),
+                    covariates = list(L = c("L_1", "L_2"),
+                                      C = c("C_1", "C_2")),
+                    utility = c("U_1", "U_2", "U_3"))
+
   qv <- policy_learn(type = "rqvl",
                      qv_models = q_glm(formula = ~ .))
+
+  expect_error(
+    qv(policy_data = pd, g_models = g_glm(), q_models = q_glm()),
+    NA
+  )
+
   expect_error(policy_eval(policy_data = pd,
                           policy_learn = qv), NA)
 
