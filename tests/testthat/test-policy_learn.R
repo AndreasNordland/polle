b983c4016dc56b7a3e6ef298586f8548d01a97e3
Andreas-Nordland <72131629+Andreas-Nordland@users.noreply.github.com>
2022-11-01 11:23:55 +0100

    Update test-policy_learn.R

--- tests/testthat/test-policy_learn.R
+++ tests/testthat/test-policy_learn.R
@@ -5,8 +5,7 @@
 
 ## QV-learning -------------------------------------------------------------
 
-test_that("input to policy_learn with type rqvl handles incorrect arguments",{
-
+test_that("policy_learn works as intended",{
   source(system.file("sim", "two_stage.R", package="polle"))
   d <- sim_two_stage(2e3, seed=1)
   pd <- policy_data(d,
@@ -25,7 +24,19 @@ test_that("input to policy_learn with type rqvl handles incorrect arguments",{
   )
 
   expect_error(policy_eval(policy_data = pd,
-                          policy_learn = qv), NA)
+                           policy_learn = qv), NA)
+})
+
+test_that("input to policy_learn with type rqvl handles incorrect arguments",{
+
+  source(system.file("sim", "two_stage.R", package="polle"))
+  d <- sim_two_stage(2e3, seed=1)
+  pd <- policy_data(d,
+                    action = c("A_1", "A_2"),
+                    baseline = c("BB", "B"),
+                    covariates = list(L = c("L_1", "L_2"),
+                                      C = c("C_1", "C_2")),
+                    utility = c("U_1", "U_2", "U_3"))
 
   qv <- policy_learn(type = "rqvl",
                      qv_models = q_glm(formula = Y ~ .))
