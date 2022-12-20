4252a9238f5a2cf268ffcdeb71c253e05fe3156d
Andreas-Nordland <72131629+Andreas-Nordland@users.noreply.github.com>
2022-12-20 11:20:42 +0100

    g_models & q_models: formula input checks

--- tests/testthat/test-g_models.R
+++ tests/testthat/test-g_models.R
@@ -1,3 +1,32 @@
+test_that("g_models checks formula input", {
+  source(system.file("sim", "two_stage.R", package="polle"))
+  d <- sim_two_stage(2e3, seed=1)
+  pd <- policy_data(d,
+                    action = c("A_1", "A_2"),
+                    baseline = c("BB", "B"),
+                    covariates = list(L = c("L_1", "L_2"),
+                                      C = c("C_1", "C_2")),
+                    utility = c("U_1", "U_2", "U_3"))
+
+  p_dynamic <- policy_def(
+    policy_functions = list(function(L_1) (L_1>0)*1,
+                            function(C_2) (C_2>0)*1),
+    reuse = FALSE
+  )
+
+  expect_error(policy_eval(policy_data = pd,
+                           policy = p_dynamic,
+                           g_models = g_glm(formula = A~X)), "The g-model formula ~X is invalid.")
+  expect_error(policy_eval(policy_data = pd,
+                           policy = p_dynamic,
+                           g_models = g_sl(formula = a~X)), "The g-model formula ~X is invalid.")
+  expect_error(policy_eval(policy_data = pd,
+                           policy = p_dynamic,
+                           g_models = g_rf(formula = ~X)), "The g-model formula ~X is invalid.")
+  expect_error(policy_eval(policy_data = pd,
+                           policy = p_dynamic,
+                           g_models = g_glmnet(formula = ~X)), "The g-model formula ~X is invalid.")
+})
 
 test_that("g_rf runs:",{
   source(system.file("sim", "single_stage.R", package="polle"))
--- tests/testthat/test-q_models.R
+++ tests/testthat/test-q_models.R
@@ -1,3 +1,32 @@
+test_that("q_models checks formula input", {
+  source(system.file("sim", "two_stage.R", package="polle"))
+  d <- sim_two_stage(2e3, seed=1)
+  pd <- policy_data(d,
+                    action = c("A_1", "A_2"),
+                    baseline = c("BB", "B"),
+                    covariates = list(L = c("L_1", "L_2"),
+                                      C = c("C_1", "C_2")),
+                    utility = c("U_1", "U_2", "U_3"))
+
+  p_dynamic <- policy_def(
+    policy_functions = list(function(L_1) (L_1>0)*1,
+                            function(C_2) (C_2>0)*1),
+    reuse = FALSE
+  )
+
+  expect_error(policy_eval(policy_data = pd,
+                           policy = p_dynamic,
+                           q_models = q_glm(formula = Y~X)), "The Q-model formula ~X is invalid.")
+  expect_error(policy_eval(policy_data = pd,
+                           policy = p_dynamic,
+                           q_models = q_sl(formula = res~X)), "The Q-model formula ~X is invalid.")
+  expect_error(policy_eval(policy_data = pd,
+                           policy = p_dynamic,
+                           q_models = q_rf(formula = V_res~X * (.))))
+  expect_error(policy_eval(policy_data = pd,
+                           policy = p_dynamic,
+                           q_models = q_glmnet(formula = Y~X)), "The Q-model formula ~X is invalid.")
+})
 
 test_that("q_rf formats data correctly via the formula",{
   source(system.file("sim", "single_stage.R", package="polle"))
