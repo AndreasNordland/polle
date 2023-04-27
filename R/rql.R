6468dfb04f012c821af6cea2f0766f4a5e93b90e
Andreas-Nordland <72131629+Andreas-Nordland@users.noreply.github.com>
2023-04-27 11:37:50 +0200

    added get_policy_functions.ql

--- R/rql.R
+++ R/rql.R
@@ -6,6 +6,7 @@ rql<- function(policy_data, alpha,
   K <- get_K(policy_data)
   n <- get_n(policy_data)
   action_set <- get_action_set(policy_data)
+  stage_action_sets <- get_stage_action_sets(policy_data)
 
   # input checks:
   if (L != 1)
@@ -31,7 +32,9 @@ rql<- function(policy_data, alpha,
   # fitting the g-functions if alpha > 0:
   if (alpha != 0){
     if (is.null(g_functions)){
-      g_functions <- fit_g_functions(policy_data, g_models = g_models, full_history = g_full_history)
+      g_functions <- fit_g_functions(policy_data,
+                                     g_models = g_models,
+                                     full_history = g_full_history)
     }
     g_values <- predict(g_functions, policy_data)
   }
@@ -82,12 +85,20 @@ rql<- function(policy_data, alpha,
   class(q_functions) <- "nuisance_functions"
   attr(q_functions, "full_history") <- q_full_history
 
+  # checking if deterministic rewards has been applied:
+  deterministic_rewards <- policy_data[["colnames"]][["deterministic_rewards"]]
+  indicator_deterministic_rewards <- !all(policy_data[["stage_data"]][,deterministic_rewards, with = FALSE] == 0)
+
+
   out <- list(
     q_functions = q_functions,
     g_functions = g_functions,
     action_set = action_set,
+    stage_action_sets = stage_action_sets,
     alpha = alpha,
-    K = K
+    K = K,
+    indicator_deterministic_rewards = indicator_deterministic_rewards,
+    deterministic_rewards = deterministic_rewards
   )
   out <- remove_null_elements(out)
   class(out) <- c("ql", "policy_object", "list")
@@ -149,4 +160,100 @@ get_policy.ql <- function(object){
   policy <- new_policy(policy, name = "ql")
 
   return(policy)
+}
+
+#' @rdname get_policy_functions
+#' @export
+get_policy_functions.ql <- function(object, stage, include_g_values = FALSE){
+  action_set <- getElement(object, "action_set")
+  stage_action_sets <- getElement(object, "stage_action_sets")
+  stage_action_set <- stage_action_sets[[stage]]; rm(stage_action_sets)
+  K <- getElement(object, "K")
+  if(!((stage >= 0) & (stage <= K)))
+    stop("stage must be smaller than or equal to K.")
+  alpha <- getElement(object, "alpha")
+
+  # getting the g-functions:
+  g_functions <- getElement(object, "g_functions")
+  if (!is.null(g_functions)){
+    g_full_history <- attr(g_functions, "full_history")
+    if (length(g_functions) == K){
+      g_function <- g_functions[[stage]]
+    }
+    else{
+      g_function <- g_functions[[1]]
+    }
+  }
+
+  # getting the (residual) Q-functions:
+  q_functions <- getElement(object, "q_functions")
+  q_model <- getElement(q_functions[[stage]], "q_model")
+  AH_names <- getElement(q_functions[[stage]], "AH_names")
+  full_history <- attr(q_functions, "full_history")
+
+  # getting the names of the deterministic reward variables:
+  deterministic_rewards <- getElement(object, "deterministic_rewards")
+  indicator_deterministic_rewards <- getElement(object, "indicator_deterministic_rewards")
+
+  stage_policy <- function(H){
+    # adding zero valued deterministic reward columns if missing
+    if (all(!(deterministic_rewards %in% colnames(H)))){
+      if (indicator_deterministic_rewards == TRUE)
+        warning("deterministic reward variables are missing in 'H' - the default value is set to zero.")
+      H[, deterministic_rewards] <- 0
+    }
+    if (!all(deterministic_rewards %in% colnames(H))){
+      tmp <- deterministic_rewards[!(deterministic_rewards %in% colnames(H))]
+      H[, tmp] <- 0
+      rm(tmp)
+    }
+
+    # (residual) Q-model predictions for the stage action set:
+    q_values <- sapply(
+      stage_action_set,
+      function(a) predict(q_model, new_AH = cbind(A = a, H))
+    )
+
+    # adding the deterministic rewards
+    q_values <- H[, deterministic_rewards[action_set %in% stage_action_set], with = FALSE] + q_values
+    colnames(q_values) <- stage_action_set
+
+    # getting
+    g_values <- NULL
+    if (alpha == 0){
+      dd <- apply(q_values,
+                  MARGIN = 1,
+                  which.max)
+      d <- stage_action_set[dd]
+    } else{
+      # evaluating the g-function:
+      if (!all(g_function$H_names %in% names(H))){
+        mes <- paste(
+          "H must contain the columns",
+          paste(g_function$H_names, collapse = ","),
+          "."
+        )
+        stop(mes)
+      }
+      g_values <- predict(g_function$g_model, new_H = H)
+
+      # calculating the realistic actions:
+      realistic_actions <- t(apply(g_values, MARGIN = 1, function(x) x >= alpha))
+      if (any(apply(realistic_actions, MARGIN = 1, sum) == 0))
+        stop("Cases with no realistic actions occur. Consider resetting the alpha level.")
+      realistic_actions[which(realistic_actions == FALSE)] <- NA
+
+      # getting the action with the maximal realistic Q-function value:
+      dd <- apply(q_values * realistic_actions, MARGIN = 1, which.max)
+      d <- stage_action_set[dd]
+    }
+
+    # including the g_values as attributes:
+    if (include_g_values == TRUE)
+      attr(d, "g_values") <- g_values
+
+    return(d)
+  }
+
+  return(stage_policy)
 }
--- tests/testthat/test-policy_learn_ql.R
+++ tests/testthat/test-policy_learn_ql.R
@@ -21,6 +21,331 @@ test_that("get_policy.ql returns a policy", {
 
 })
 
+test_that("get_policy_functions.ql agrees with get_policy.ql", {
+  d <- sim_two_stage(500, seed=1)
+  pd <- policy_data(d,
+                    action = c("A_1", "A_2"),
+                    baseline = c("BB", "B"),
+                    covariates = list(L = c("L_1", "L_2"),
+                                      C = c("C_1", "C_2")),
+                    utility = c("U_1", "U_2", "U_3"))
+
+  # not full history:
+  pl <- policy_learn()
+  po <- pl(pd, q_models = q_glm())
+
+  pf_1 <- get_policy_functions(po, stage = 1)
+  his_1 <- get_history(pd, stage = 1,
+                       full_history = FALSE)
+  H1 <- his_1$H[, -c("id", "stage"), with = FALSE]
+  pa_1 <- pf_1(H1)
+
+  pf_2 <- get_policy_functions(po, stage = 2)
+  his_2 <- get_history(pd, stage = 2,
+                       full_history = FALSE)
+  H2 <- his_2$H[, -c("id", "stage"), with = FALSE]
+  H2 <- cbind(H2, his_2$U[, -c("id", "stage", "U_bar"), with = FALSE])
+  pa_2 <- pf_2(H2)
+
+  ## benchmark
+  pa <- get_policy(po)(pd)
+
+  expect_equal(
+    pa[stage ==1]$d,
+    pa_1
+  )
+  expect_equal(
+    pa[stage ==2]$d,
+    pa_2
+  )
+
+  # full history:
+  pl <- policy_learn()
+  po <- pl(pd, q_models = q_glm(), q_full_history = TRUE)
+  pf_1 <- get_policy_functions(po, stage = 1)
+  his_1 <- get_history(pd, stage = 1,
+                       full_history = TRUE)
+  H1 <- his_1$H[, -c("id", "stage"), with = FALSE]
+  H1 <- cbind(H1, his_1$U[, -c("id", "stage", "U_bar"), with = FALSE])
+  pa_1 <- pf_1(H1)
+
+  pf_2 <- get_policy_functions(po, stage = 2)
+  his_2 <- get_history(pd, stage = 2,
+                       full_history = TRUE)
+  H2 <- his_2$H[, -c("id", "stage"), with = FALSE]
+  H2 <- cbind(H2, his_2$U[, -c("id", "stage", "U_bar"), with = FALSE])
+  pa_2 <- pf_2(H2)
+
+  ## benchmark
+  pa <- get_policy(po)(pd)
+
+  expect_equal(
+    pa[stage ==1]$d,
+    pa_1
+  )
+  expect_equal(
+    pa[stage ==2]$d,
+    pa_2
+  )
+})
+
+test_that("get_policy_functions.ql handles deterministic rewards", {
+  d <- sim_two_stage(500, seed=1, deterministic_rewards = TRUE)
+  pd <- policy_data(d,
+                    action = c("A_1", "A_2"),
+                    baseline = c("BB", "B"),
+                    covariates = list(L = c("L_1", "L_2"),
+                                      C = c("C_1", "C_2")),
+                    utility = c("U_1", "U_2", "U_3"),
+                    deterministic_rewards = list(
+                      U_A0 = c("U_1_A0", "U_2_A0"),
+                      U_A1 = c("U_1_A1", "U_2_A1")
+                    ))
+
+  # not full history:
+  pl <- policy_learn()
+  po <- pl(pd, q_models = q_glm())
+
+  pf_1 <- get_policy_functions(po, stage = 1)
+  his_1 <- get_history(pd, stage = 1,
+                       full_history = FALSE)
+  H1 <- his_1$H[, -c("id", "stage"), with = FALSE]
+  H1 <- cbind(H1, his_1$U[, -c("id", "stage", "U_bar"), with = FALSE])
+  pa_1 <- pf_1(H1)
+
+  pf_2 <- get_policy_functions(po, stage = 2)
+  his_2 <- get_history(pd, stage = 2,
+                       full_history = FALSE)
+  H2 <- his_2$H[, -c("id", "stage"), with = FALSE]
+  H2 <- cbind(H2, his_2$U[, -c("id", "stage", "U_bar"), with = FALSE])
+  pa_2 <- pf_2(H2)
+
+  ## benchmark
+  pa <- get_policy(po)(pd)
+
+  expect_equal(
+    pa[stage ==1]$d,
+    pa_1
+  )
+  expect_equal(
+    pa[stage ==2]$d,
+    pa_2
+  )
+
+  # full history:
+  pl <- policy_learn()
+  po <- pl(pd, q_models = q_glm(), q_full_history = TRUE)
+  pf_1 <- get_policy_functions(po, stage = 1)
+  his_1 <- get_history(pd, stage = 1,
+                       full_history = TRUE)
+  H1 <- his_1$H[, -c("id", "stage"), with = FALSE]
+  H1 <- cbind(H1, his_1$U[, -c("id", "stage", "U_bar"), with = FALSE])
+  pa_1 <- pf_1(H1)
+
+  pf_2 <- get_policy_functions(po, stage = 2)
+  his_2 <- get_history(pd, stage = 2,
+                       full_history = TRUE)
+  H2 <- his_2$H[, -c("id", "stage"), with = FALSE]
+  H2 <- cbind(H2, his_2$U[, -c("id", "stage", "U_bar"), with = FALSE])
+  pa_2 <- pf_2(H2)
+
+  ## benchmark
+  pa <- get_policy(po)(pd)
+
+  expect_equal(
+    pa[stage ==1]$d,
+    pa_1
+  )
+  expect_equal(
+    pa[stage ==2]$d,
+    pa_2
+  )
+})
+
+test_that("get_policy_functions.ql handles partially missing deterministic rewards", {
+  d <- sim_two_stage(500, seed=1, deterministic_rewards = TRUE)
+  pd <- policy_data(d,
+                    action = c("A_1", "A_2"),
+                    baseline = c("BB", "B"),
+                    covariates = list(L = c("L_1", "L_2"),
+                                      C = c("C_1", "C_2")),
+                    utility = c("U_1", "U_2", "U_3"),
+                    deterministic_rewards = list(
+                      U_A0 = c("U_1_A0", "U_2_A0"),
+                      U_A1 = c("U_1_A1", "U_2_A1")
+                    ))
+
+  # not full history:
+  pl <- policy_learn()
+  po <- pl(pd, q_models = q_glm())
+
+  pf_1 <- get_policy_functions(po, stage = 1)
+  his_1 <- get_history(pd, stage = 1,
+                       full_history = FALSE)
+  H1 <- his_1$H[, -c("id", "stage"), with = FALSE]
+  H1 <- cbind(H1, his_1$U[, -c("id", "stage", "U_bar", "U_A0"), with = FALSE])
+  pa_1 <- pf_1(H1)
+
+  pf_2 <- get_policy_functions(po, stage = 2)
+  his_2 <- get_history(pd, stage = 2,
+                       full_history = FALSE)
+  H2 <- his_2$H[, -c("id", "stage"), with = FALSE]
+  H2 <- cbind(H2, his_2$U[, -c("id", "stage", "U_bar", "U_A0"), with = FALSE])
+  pa_2 <- pf_2(H2)
+
+  ## benchmark
+  pa <- get_policy(po)(pd)
+
+  expect_equal(
+    pa[stage ==1]$d,
+    pa_1
+  )
+  expect_equal(
+    pa[stage ==2]$d,
+    pa_2
+  )
+
+  # full history:
+  pl <- policy_learn()
+  po <- pl(pd, q_models = q_glm(), q_full_history = TRUE)
+  pf_1 <- get_policy_functions(po, stage = 1)
+  his_1 <- get_history(pd, stage = 1,
+                       full_history = TRUE)
+  H1 <- his_1$H[, -c("id", "stage"), with = FALSE]
+  H1 <- cbind(H1, his_1$U[, -c("id", "stage", "U_bar", "U_A0"), with = FALSE])
+  pa_1 <- pf_1(H1)
+
+  pf_2 <- get_policy_functions(po, stage = 2)
+  his_2 <- get_history(pd, stage = 2,
+                       full_history = TRUE)
+  H2 <- his_2$H[, -c("id", "stage"), with = FALSE]
+  H2 <- cbind(H2, his_2$U[, -c("id", "stage", "U_bar", "U_A0"), with = FALSE])
+  pa_2 <- pf_2(H2)
+
+  ## benchmark
+  pa <- get_policy(po)(pd)
+
+  expect_equal(
+    pa[stage ==1]$d,
+    pa_1
+  )
+  expect_equal(
+    pa[stage ==2]$d,
+    pa_2
+  )
+})
+
+test_that("get_policy_functions.ql handles realistic actions", {
+  d <- sim_two_stage(500, seed=1)
+  pd <- policy_data(d,
+                    action = c("A_1", "A_2"),
+                    baseline = c("BB", "B"),
+                    covariates = list(L = c("L_1", "L_2"),
+                                      C = c("C_1", "C_2")),
+                    utility = c("U_1", "U_2", "U_3"))
+
+  # not full history:
+  pl <- policy_learn(alpha = 0.1)
+  po <- pl(pd, q_models = q_glm(), g_models = g_glm())
+
+  pf_1 <- get_policy_functions(po, stage = 1)
+  his_1 <- get_history(pd, stage = 1,
+                       full_history = FALSE)
+  H1 <- his_1$H[, -c("id", "stage"), with = FALSE]
+  pa_1 <- pf_1(H1)
+
+  pf_2 <- get_policy_functions(po, stage = 2)
+  his_2 <- get_history(pd, stage = 2,
+                       full_history = FALSE)
+  H2 <- his_2$H[, -c("id", "stage"), with = FALSE]
+  H2 <- cbind(H2, his_2$U[, -c("id", "stage", "U_bar", "U_A0"), with = FALSE])
+  pa_2 <- pf_2(H2)
+
+  ## benchmark
+  pa <- get_policy(po)(pd)
+
+  expect_equal(
+    pa[stage ==1]$d,
+    pa_1
+  )
+  expect_equal(
+    pa[stage ==2]$d,
+    pa_2
+  )
+
+  # full history:
+  pl <- policy_learn(alpha = 0.1)
+  po <- pl(pd, q_models = q_glm(), q_full_history = TRUE, g_models = g_glm(), g_full_history = FALSE)
+  pf_1 <- get_policy_functions(po, stage = 1)
+  his_1 <- get_history(pd, stage = 1,
+                       full_history = TRUE)
+  H1 <- his_1$H[, -c("id", "stage"), with = FALSE]
+  H1 <- cbind(H1, his_1$U[, -c("id", "stage", "U_bar", "U_A0"), with = FALSE])
+  H1$L <- H1$L_1
+  H1$C <- H1$C_1
+  pa_1 <- pf_1(H1)
+
+  pf_2 <- get_policy_functions(po, stage = 2)
+  his_2 <- get_history(pd, stage = 2,
+                       full_history = TRUE)
+  H2 <- his_2$H[, -c("id", "stage"), with = FALSE]
+  H2 <- cbind(H2, his_2$U[, -c("id", "stage", "U_bar", "U_A0"), with = FALSE])
+  H2$L <- H2$L_2
+  H2$C <- H2$C_2
+  pa_2 <- pf_2(H2)
+
+  ## benchmark
+  pa <- get_policy(po)(pd)
+
+  expect_equal(
+    pa[stage ==1]$d,
+    pa_1
+  )
+  expect_equal(
+    pa[stage ==2]$d,
+    pa_2
+  )
+})
+
+test_that("get_policy_functions.ql handles varying stage action set", {
+  d <- sim_two_stage_multi_actions(n=500, seed=1)
+  pd <- policy_data(d,
+                    action = c("A_1", "A_2"),
+                    baseline = c("BB", "B"),
+                    covariates = list(L = c("L_1", "L_2"),
+                                      C = c("C_1", "C_2")),
+                    utility = c("U_1", "U_2", "U_3"))
+
+  # not full history:
+  pl <- policy_learn(alpha = 0.05)
+  po <- pl(pd, q_models = q_glm(), g_models = list(g_glm(), g_empir()))
+
+  pf_1 <- get_policy_functions(po, stage = 1)
+  his_1 <- get_history(pd, stage = 1,
+                       full_history = FALSE)
+  H1 <- his_1$H[, -c("id", "stage"), with = FALSE]
+  pa_1 <- pf_1(H1)
+
+  pf_2 <- get_policy_functions(po, stage = 2)
+  his_2 <- get_history(pd, stage = 2,
+                       full_history = FALSE)
+  H2 <- his_2$H[, -c("id", "stage"), with = FALSE]
+  pa_2 <- pf_2(H2)
+
+  ## benchmark
+  pa <- get_policy(po)(pd)
+
+  expect_equal(
+    pa[stage ==1]$d,
+    pa_1
+  )
+  expect_equal(
+    pa[stage ==2]$d,
+    pa_2
+  )
+})
+
+
 test_that("policy_learn with type = 'ql' works as intended",{
   d <- sim_two_stage(200, seed=1)
 
