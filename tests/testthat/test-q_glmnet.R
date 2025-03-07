e446a1e3a7aa1ab93ea4841108695fa359159224
OANO (Andreas Nordland) <andreasnordland@gmail.com>
2025-03-07 12:54:47 +0100

    added test for q_glmnet: prediction associated with the given s argument

--- tests/testthat/test-q_glmnet.R
+++ tests/testthat/test-q_glmnet.R
@@ -110,3 +110,73 @@ test_that("q_glmnet handles missing covariates", {
         )
     )
 })
+
+test_that("testing that q_glmnet returns the prediction associated with lambda.min", {
+
+  par0 <- list(a = 0, b = 0.0, c = 0)
+  sim_d <- function(n,
+                    par = par0,
+                    potential_outcomes = FALSE,
+                    action_function = function(H){
+                      n <- nrow(H)
+                      rbinom(n = n, size = 1, prob = 0.5)
+                    }) {
+    W <- runif(n = n, min = -1, max = 1)
+    L <- runif(n = n, min = -1, max = 1)
+    H <- data.table(W = W, L = L)
+    A <- as.numeric(action_function(H))
+    U1 <- W + L + (par$c*W + par$a*L + par$b) # U^1
+    U0 <- W + L # U^0
+    U <- A * U1 + (1 - A) * U0 + rnorm(n = n)
+    out <- data.table(W = W, L = L, A = A, U = U)
+    if (potential_outcomes == TRUE) {
+      out$U0 <- U0
+      out$U1 <- U1
+    }
+    return(out)
+  }
+
+  set.seed(1)
+  d<-sim_d(400)
+
+  pd <- policy_data(
+    d,
+    action = "A",
+    covariates = list("W", "L"),
+    utility = "U"
+  )
+  his <- get_history(pd)
+
+  pl <- policy_learn(
+    type = "blip",
+    control = control_blip(blip_models = q_glmnet(~.)),
+    threshold=0
+  )
+
+  po <- pl(
+    policy_data = pd,
+    g_models = g_glm(~1),
+    q_models = q_glmnet(~.*A)
+  )
+
+  qf <- get_q_functions(po)
+
+  ## fitted values of the Q-function using s = "lambda.min"
+  expect_equal(
+    qf$stage_1$q_model$s,
+    "lambda.min"
+  )
+  pred_lambda.min <- predict(qf, pd)
+
+  ## predictions from a cv.glmnet object uses s = "lambda.1se" per default
+  ## see https://cran.r-project.org/web/packages/glmnet/glmnet.pdf#page=59
+  m_glmnet <- qf$stage_1$q_model$model
+  ## applying the design (formula) to the action and history matrix:
+  pred_data <- apply_design(design = qf$stage_1$q_model$design, data = cbind(A = "1", his$H))
+  pred_lambda.min_ref <- predict(qf$stage_1$q_model$model, newx = pred_data, s = "lambda.min")
+
+  expect_equal(
+    pred_lambda.min$Q_1,
+    pred_lambda.min_ref[,1] |> unname()
+  )
+})
