7264102b39626511ac1f4ea00a4ec7b59b148e78
Andreas Nordland <andreasnordland@gmail.com>
2026-09-22 16:02:31 +0200

    init bug fix

--- R/Q_function.R
+++ R/Q_function.R
@@ -304,15 +304,13 @@ fit_Q_functions <- function(policy_data,
 
   ## (n X K+1) matrix with entries
   ## k-column = Q_k(H_{k,i}, d_k(H_{k,i})), k = 1,...,K
-  ## K+1-column = U (if no missing final outcomes) or Q_{K+1}(H_{K+1})
-  Q <- matrix(nrow = n, ncol = K + 1)
-  if (is.null(m_function) == TRUE){
-    ## getting the observed (complete) utility:
-    utility <- get_utility(policy_data)
-    ## (n) vector with entries U_i:
-    U <- utility$U
-    Q[, K + 1] <- U
-  } else {
+  ## K+1-column = U (if no missing final outcomes)
+  ## or Q_{K+1}(H_{K+1}) (equals U for early terminal events)
+  Q <- matrix(NA_real_, nrow = n, ncol = K + 1)
+  utility <- get_utility(policy_data) # observed (complete) utility
+  Q[, K + 1] <- utility[["U"]]
+
+  if (!is.null(m_function)){
     Q_K1 <- predict(m_function, new_policy_data = policy_data)
     Q[(id %in% Q_K1[["id"]]), K + 1] <- Q_K1[["Q"]]
   }
